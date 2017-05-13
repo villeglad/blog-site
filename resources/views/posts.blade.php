add2c4a7b0889d3e9915bbca95deca5787b566d2
Ville Glad <ville.j.glad@gmail.com>
2017-05-13 13:42:50 +0300

    Edit posts view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -21,75 +21,22 @@
             margin: 0;
         }
 
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
     </style>
 </head>
 <body>
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @if (Auth::check())
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ url('/login') }}">Login</a>
-                <a href="{{ url('/register') }}">Register</a>
-            @endif
-        </div>
-    @endif
+<div>
 
-    <div class="content">
-        <div class="title m-b-md">
-            Posts
-        </div>
+    <div>
+        <h1>Posts</h1>
 
-       <ul>
+        <ul>
            @foreach($posts as $post)
                <li>
                    <h3>{{$post->title}}</h3>
                    <p>{{$post->body}}</p>
                </li>
            @endforeach
-       </ul>
+        </ul>
     </div>
 </div>
 </body>
